17beb0522f42c6637ed02c6b3cd472aa485ae2b7
Laura-Linares <lauralinarescontacto@gmail.com>
2025-10-29 19:50:50 +0100

    script packages_adm working with log messages and getopt

---
+++ log_functions.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#-------------------------------------------
+# Archivo: /lib/myfunctions/log_functions.sh
+#   Autor: Laura Linares
+# Descripción: Librería de funciones de log personalizadas
+#-------------------------------------------
+
+# Declaracion de variables
+LOGFILE="/var/log/scripts_laura.log"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+# Funcion que cambia el .log donde se almacenarán los logs
+set_logfile() {
+    LOGFILE="$1"
+}
+
+# Función para que salga el mensaje por pantalla y que se escriba en el .log
+_log() {
+    local type="$1"
+    local color="$2"
+    local message="$3"
+    local timestamp
+    timestamp=$(date '+%Y-%m-%d_%H:%M:%S')
+
+    # Salida por pantalla
+    echo -e "${color}[$timestamp] [$type] $message${NC}"
+
+    # Escritura en .log
+    echo "[$timestamp] [$type] $message" >> "$LOGFILE"
+}
+
+# Funciones con los distintos logs posibles
+log_success() {
+    _log "OK" "$GREEN" "$1";
+}
+log_error() {
+    _log "ERROR" "$RED" "$1";
+}
+log_info() {
+    _log "INFO" "$BLUE" "$1";
+}
--- packages_adm2.sh
+++ packages_adm2.sh
@@ -1,33 +1,37 @@
 #! /bin/bash
+#---------------------------------
+# Archivo: packages_adm.sh
+# Autor: Laura Linares
+# Version: 3.1
+#---------------------------------
+# Uso: crea directorios, descarga y descomprime ficheros pasados mediante enlace,
+# comprime una carpeta de trabajo para crear una copia de seguridad y recupera
+# copias de segurdad realizadas previamente en el directorio de trabajo especificado
+#---------------------------------
 
-#----Author:LauraLinares-----
-#---------Version:2.0--------
-#---Script:packages_adm2.sh---
+# Carga funciones personalizadas para log
+. /lib/myfunctions/log_functions.sh
 
-#---Uso: upgrade de packages_adm.sh haciendo uso de getopt---
-#--Script para la creación de directorios, descarga de enlaces y su posterior descompresión, 
-#--compresión de una carpeta de trabajo para la creación de una copia de seguridad y/o la---
-#--recuperación de una copia de seguridad realizada previamente-----------------------------
-
-
-#--Declaración de funciones--
+# Declaración de funciones
 usage() {
     cat << EOF
-Uso: $0 [OPCIONES] [ARGUMENTOS]
+$0: $0 [opción] [argumento]
 
-Script para la creación de directorios, descarga de enlaces y su posterior descompresión, compresión de una carpeta de trabajo para la creación de una copia de seguridad y/o la recuperación de una copia de seguridad realizada previamente.
+    Crea directorios de trabajo en /opt.
+    Descarga y descomprime archivos pasados mediante enlace.
+    Cierra un directorio de trabajo creando una copia de seguridad en /usr/local/lib
+    Recupera y descomprime un directorio de trabajo previamente cerrado
 
-Opciones:
-    -a, --accion ACCION           Acción que se realizará (obligatorio)
-    -e, --enlace ENLACE           Enlace con el archivo comprimido para descargar
-    -d, --directorio DIRECTORIO   Directorio de trabajo en /opt
-    -h, --help                    Muestra este mensaje de ayuda
+    Opciones:
+        [obligatorio]   -a | --accion       indica la acción que debe realizar este script entre las disponibles
 
-Ejemplos:
-    $0 -a nuevaconfig -d apache
-    $0 -a descargar -d apache -e https://....
-    $0 --accion cerrar --directorio apache
-    $0 -a recuperar -d apache
+        [obligatorio]   -d | --directorio   indica el directorio con el que se va a trabajar
+
+                        -e | --enlace       enlace desde el que se descargará el fichero comprimido
+
+                        -h | --help         proporciona este mensaje de ayuda
+    
+    Los log se guardarán en un fichero ubicado en /var/log/scripts_laura.sh
 EOF
     exit 1
 }
@@ -35,51 +39,51 @@ EOF
 last_error_control() {
     #Controla que el último proceso haya sido exitoso
     if [ $? -eq 0 ]; then
-        echo "La acción se ha completado sin fallos"
+        log_success "La acción se ha completado sin fallos"
     else
-        echo "Ha ocurrido un error durante la ejecución de esta acción"
+        log_error "Ha ocurrido un error durante la ejecución de esta acción"
         exit 1
     fi
 }
 
 check_accion() {
     if [ -z "$accion" ]; then
-        echo "Error: Debe especificarse una acción con -a o --accion" >&2
+        log_info "Debe especificarse una acción con -a o --accion" >&2
         usage
     fi
 }
 
 check_directorio() {
     if [ -z "$directorio" ]; then
-        echo "Error: Para esta acción debe especificarse el nombre de un directorio con -d o --directorio" >&2
+        log_info "Para esta acción debe especificarse el nombre de un directorio con -d o --directorio" >&2
         usage
     fi
 }
 
 check_enlace() {
     if [ -z "$enlace" ]; then
-        echo "Error: Para esta acción debe especificarse un enlace para la descarga con -e o --enlace" >&2
+        log_info "Para esta acción debe especificarse un enlace para la descarga con -e o --enlace" >&2
         usage
     fi
 }
 
-#--Declaración de variables--
+# Declaración de variables
 accion=""
 enlace=""
 directorio=""
 
-#------Inicio del script-----
-    #Analiza opciones de línea de comandos
+#----------------------Inicio del script----------------------
+    # Analiza opciones de línea de comandos
 OPTS=$(getopt -o a:e:d:h --long accion:,enlace:,directorio:,help -- "$@")
 
 if [ $? -ne 0 ]; then
-    echo "Error al analizar opciones" >&2
+    log_info "No se han podido analizar las opciones proporcionadas" >&2
     usage
 fi
 
 eval set -- "$OPTS"
 
-    #Procesa las opciones
+    # Procesa las opciones
 while true; do
     case "$1" in
         -a | --accion)
@@ -108,126 +112,107 @@ while true; do
     esac
 done
 
-    #Controla que se pase la opcion obligatoria (accion)
+    # Controla que se pasen las opciones obligatorias
 check_accion
+check_directorio
 
-    #Manejo de las distintas opciones
+    # Manejo de las distintas opciones
 case "$accion" in
-    nuevaconfig | nuevaconfiguracion)
-        check_directorio
-
-            #Comprueba si el directorio existe
+    nueva | nuevaconfig | nuevaconfiguracion)
+            # Comprueba si el directorio existe
         if [ -d "/opt/$directorio" ]; then
-            echo "Error, el directorio \"$directorio\" que está intentando crear, ya existe"
+            log_error "El directorio que está intentando crear ya existe"
             exit 1
         fi
-
-            #Crea el directorio
+            # Crea el directorio
         sudo mkdir /opt/$directorio
-
+            # Comprueba errores en la ejecución
         last_error_control
         ;;
     descargar)
-        check_directorio
+            # Comprueba que se ha pasado un enlace
         check_enlace
-
-                #Comprueba si existe el directorio donde desea descargar
+            # Comprueba si existe el directorio donde desea descargar
         if [ -d "/opt/$directorio" ]; then
-
-            #Comprueba que los paquetes necesarios estén instalados
+            # Comprueba que los paquetes necesarios estén instalados
             for i in wget gzip bzip2; do
                 if ! command -V "$i" >/dev/null 2>&1; then
-                    echo "Instalando $i"
+                    log_info "Instalando $i"
                     sudo apt-get update -qq >/dev/null 2>&1
                     sudo apt-get install -y -qq "$i" >/dev/null 2>&1
                 fi
             done
-
-                #Obtiene el nombre del fichero desde la URL
+                # Obtiene el nombre del fichero desde la URL
             fichero=$(basename "$enlace")
-                #Almacena en una variable la ruta que tendría el fichero
+                # Almacena en una variable la ruta que tendría el fichero
             ruta_fichero="/opt/$directorio/$fichero"
-
-            #Comprueba si el fichero ya existe
+                # Comprueba si el fichero ya existe
             if [ -f "$ruta_fichero" ]; then
-                echo "Error, el fichero que intenta descargar ya existe en ese directorio"
+                log_error "El fichero que intenta descargar ya existe en ese directorio"
                 exit 1
             fi
-
-                #Descarga el archivo en el directorio
+                # Descarga el archivo en el directorio
             sudo wget -q -P "/opt/$directorio" "$enlace"
-
-                #Comprueba que el archivo descargado existe
+                # Comprueba que el archivo descargado existe
             if [ ! -f "$ruta_fichero" ]; then
-                echo "Error, no se ha encontrado el archivo descargado $fichero"
+                log_error "No se ha encontrado el archivo descargado"
                 exit 1
             fi
-
-                #Lo descomprime
+                # Lo descomprime
             if [[ "$fichero" == *.tar.gz ]]; then
                 sudo tar -xzf "$ruta_fichero" -C "/opt/$directorio"
             elif [[ "$fichero" == *.tar.bz2 ]]; then
                 sudo tar -xjf "$ruta_fichero" -C "/opt/$directorio"
             else
-                echo "Archivo descargado, pero no es .tar.gz ni .tar.bz2"
-                echo "No se ha podido descomprimir"
+                log_error "Archivo descargado, pero no es .tar.gz ni .tar.bz2 -- No se ha podido descomprimir"
                 exit 1
             fi
-
+                # Comprueba errores en la ejecución
             last_error_control
-
         else
-            echo "Error, el directorio no existe. Créelo primero"
+            log_error "El directorio indicado no existe. Debe crearlo primero para poder descargar contenido en él"
             exit 1
         fi
         ;;
     cerrar)
-        check_directorio
         if [ -d "/opt/$directorio" ]; then
+                # Crea el nombre que tendrá el archivo con la fecha del momento en que se ejecuta la acción
             nombre_archivo="${directorio}_$(date +%Y_%m_%d).tar.gz"
             dir_destino="/usr/local/lib"
-
-                #Crea el archivo en el directorio de destino
+                # Crea el archivo en el directorio de destino
             sudo tar -czf "$dir_destino/$nombre_archivo" -C "/opt" "$directorio"
-
+                # Comprueba errores en la ejecución
             last_error_control
-
         else
-            echo "Error, el directorio $directorio no existe"
+            log_error "El directorio indicado no existe"
             exit 1
         fi
         ;;
     recuperar)
-        check_directorio
-            #Busca la o las copias que haya con ese nombre
+            # Busca la o las copias que haya con el nombre especificado
         copias=$(find /usr/local/lib -maxdepth 1 -type f -name "$directorio*")
-
-            #Controla la existencia de alguna copia de seguridad
+            # Controla la existencia de alguna copia de seguridad
         if [ -z "$copias" ]; then
-            echo "Error, no existe ninguna copia de seguridad sobre esa configuración"
+            log_error "No existe ninguna copia de seguridad de esa configuración"
             exit 1
         else
-                #Comprueba si existe el directorio donde se va a recuperar
+                # Comprueba si existe el directorio donde se va a recuperar
             if [ ! -d "/opt/$directorio" ]; then
-                echo "Error, no existe el directorio $directorio y no se podrá guardar la copia de seguridad allí"
-                echo "Creelo usando el USO 1 de este script"
-                exit 1
+                log_info "No existe el directorio de destino. Se va a crear para poder continuar"
+                sudo mkdir "/opt/$directorio"
             fi
-
-                #Ordena las copias y coge solo la última
+                # Ordena las copias y coge solo la última
             archivo=$(echo $copias | tr " " "\n" | sort -nr | head -1)
-
-                #Borra el contenido del directorio de destino
+                # Borra el contenido del directorio de destino
             destino="/opt/$directorio"
             sudo rm -r $destino/*
-
-                #Extrae el contenido en el directorio de destino
+                # Extrae el contenido en el directorio de destino
             sudo tar -xzf "$archivo" -C "/opt/$directorio"
-
+                # Comprueba errores en la ejecución
             last_error_control
         fi
         ;;
     *)
-        echo "Error, la acción que ha introducido no ha sido encontrada"
+        log_error "La acción que ha introducido no ha sido encontrada"
         ;;
 esac
